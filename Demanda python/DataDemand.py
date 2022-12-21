b99c65cadb9378ffe35142fefc404298e027c27b
Nico Quintero <juan24092002@gmail.com>
2022-12-21 18:08:09 -0500

    Cambio de librerias
    Se añade pandas para lectura y creación de tablas en Demanda

--- Demanda python/DataDemand.py
+++ Demanda python/DataDemand.py
@@ -1,12 +1,12 @@
-from readxlxs import xlxstocsv
 import csv
 import datetime
 import openpyxl
+import pandas as pd
    
 importedfile = openpyxl.load_workbook(filename = 'DemandaZonas.xlsx', read_only = True, keep_vba = False)
 tabnames = importedfile.sheetnames 
 
-###############################################################################
+##############################################################################
 def is_empty(any_structure):
     if any_structure:
         return False
@@ -14,10 +14,11 @@ def is_empty(any_structure):
         return True
        
 ###########################################################################
-substring = "DemandaMensual"
-xlxstocsv(tabnames,substring,importedfile)
 
-with open('temp/DemandaMensual.csv') as csvfile:
+read_file = pd.read_excel ('DemandaZonas.xlsx', sheet_name='DemandaMensual')
+read_file.to_csv ('DemandaMensual.csv', index = None, header=True)
+
+with open('DemandaMensual.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     dmdData = [[] for x in range(4)];  
     for row in readCSV:
@@ -30,13 +31,15 @@ with open('temp/DemandaMensual.csv') as csvfile:
                 pass
             dmdData[col].append(val)
 for col in range(4):
-    dmdData[col].pop(0) 
+    dmdData[col].pop(0)
 
-###########################################################################
-substring = "Reservas"
-xlxstocsv(tabnames,substring,importedfile)
 
-with open('temp/Reservas.csv') as csvfile:
+# ###########################################################################
+
+read_file = pd.read_excel ('DemandaZonas.xlsx', sheet_name='Reservas')
+read_file.to_csv ('Reservas.csv', index = None, header=True)
+
+with open('Reservas.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     dmdDataR = [];  
     for row in readCSV:
@@ -49,53 +52,50 @@ with open('temp/Reservas.csv') as csvfile:
                 pass
             dmdDataR.append(val)
     
-###########################################################################
+# ###########################################################################
 
-substring = "Horario"
-xlxstocsv(tabnames,substring,importedfile)
-             
-with open('temp/Horario.csv') as csvfile:
+read_file = pd.read_excel ('DemandaZonas.xlsx', sheet_name='Horario')
+read_file = read_file.drop(read_file.columns[0], axis=1)
+read_file.to_csv ('Horario.csv', index = None, header=True)
+
+
+with open('Horario.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
-    for row in readCSV: columns = len(row); break                
-with open('temp/Horario.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    horaData = [[] for x in range(columns-1)];  
+    horaData = [[] for x in range(16)];  
     for row in readCSV:
         if is_empty(row) is True: break 
-        for col in range(columns-1):
-            val = row[col+1]
+        for col in range(16):
+            val = row[col]
             try: 
-                val = float(row[col+1])
+                val = float(row[col])
             except ValueError:
                 pass
             horaData[col].append(val)
 # for col in range(columns-1):
 #     horaData[col].pop(0) 
 
-###########################################################################
+# ###########################################################################
 
-substring = "Zonas"
-xlxstocsv(tabnames,substring,importedfile)
-             
-with open('temp/Zonas.csv') as csvfile:
+read_file = pd.read_excel ('DemandaZonas.xlsx', sheet_name='Zonas')
+read_file = read_file.drop(read_file.columns[0], axis=1)
+read_file.to_csv ('Zonas.csv', index = None, header=True)
+
+with open('Zonas.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
-    for row in readCSV: columns = len(row); break                
-with open('temp/Zonas.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    zonasData = [[] for x in range(columns-1)];  
+    zonasData = [[] for x in range(15)];  
     for row in readCSV:
         if is_empty(row) is True: break 
-        for col in range(columns-1):
-            val = row[col+1]
+        for col in range(15):
+            val = row[col]
             try: 
-                val = float(row[col+1])
+                val = float(row[col])
             except ValueError:
                 pass
             zonasData[col].append(val)
 # for col in range(columns-1):
 #     zonasData[col].pop(0) 
 
-###########################################################################
+# ###########################################################################
 month=[[] for x in range(12)]
 for y in range(15):
     for m in range(12):
@@ -121,39 +121,39 @@ q=[[sum(x)/3 for x in zip(month[0], month[1], month[2])],
    [sum(x)/3 for x in zip(month[7], month[8], month[9])],
    [sum(x)/3 for x in zip(month[4], month[5], month[6])]]
 
+# Condiciones climáticas similares
 # Q1	Ene, Feb, Mar
-# Q2	Abr, Dic
-# Q3	Ago, Sept, Oct, Nov
+# Q2	Abr, Nov, Dic
+# Q3	Ago, Sept, Oct
 # Q4	May, Jun, Jul
 
 promm=[[] for x in range(4)]
-for qfila in range(4):
-    for y in range(6):
+for qfila in range(4):  #4 cuartiles
+    for y in range(6):  #6 bloques de 5 años
         val=[]
-        for m in range(5):
-            val.append(q[qfila][5*y+m])
-        promm[qfila].append(max(val))
+        for m in range(5): #Longitud de cada bloque
+            val.append(q[qfila][5*y+m]) 
+        promm[qfila].append(max(val)) #Max del prom por cuartil en esos 5 años
 
 prommlist=[]
-for y in range(6):
-    for qf in range(4):
+for y in range(6):      #6 bloques
+    for qf in range(4): #por cuartil
         prommlist.append(promm[qf][y])
 
 zondmq=[[] for x in range(15)]
-for z in range(15):
-    for y in range(24):
-        zondmq[z].append(zonasData[z][y+1]*prommlist[y])
+for z in range(15):         # Recorre las 15 zonas
+    for y in range(24):     # 24 cuartiles en total
+        zondmq[z].append(zonasData[z][y+1]*prommlist[y])  #Desagregar por zonas
 
 final=[[] for x in range(3)]
-for z in range(15):
+for z in range(15):         #15 zonas
     val=0
-    for y in range(24):
-        for x in range(24):
+    for y in range(24):     #24 cuartiles
+        for x in range(24): #24 horas
             final[0].append(horaData[z+1][0])
             final[1].append(val+1)
             val=val+1
             final[2].append(zondmq[z][y]*horaData[z+1][x+1])
-
 
 from openpyxl import Workbook
 
